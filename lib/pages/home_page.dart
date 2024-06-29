949329c90c143237a77b95a468165c4249d8fe1b
shady-ateff <113939797+shady-ateff@users.noreply.github.com>
2024-06-29 15:53:02 +0300

    Offer Banner Galary

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/widgets/offers_banners.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Color> color = [Colors.black45];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         elevation: 0, //App bar shadow
-        title: Center(
-          child: const Text('Talabak'),
+        title: const Center(
+          child: Text('Talabak'),
         ),
       ),
       drawer: Drawer(child: ListView()),
@@ -20,73 +27,9 @@ class HomePage extends StatelessWidget {
           const SizedBox(
             height: 40,
           ),
-          Stack(
-            children: [
-              Container(
-                //padding: const EdgeInsets.all(10),
-                height: 150,
-                child: PageView(
-                  //physics: const NeverScrollableScrollPhysics(),
-                  scrollDirection: Axis.horizontal,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: ClipRRect(
-                          //to clip image and make it rounded border
-                          borderRadius: BorderRadius.circular(10),
-                          child: Image.asset(
-                            'assets/images/offer2.jpg',
-                            //width: 400,
-                            //height: 200,
-                           fit: BoxFit.cover,
-                          )),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: ClipRRect(  //to clip image and make it rounded border
-                        borderRadius: BorderRadius.circular(10),
-                          child: Image.asset(
-                        'assets/images/offer1.jpg',
-                        //width: 400,
-                        //height: 200,
-                        fit: BoxFit.cover,
-                      )),
-                    ),
-                  ],
-                ),
-              ),
-              Center(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    CustomPaint(painter: CirclePainter(),)
-                  ]
-                )
-              )
-            ],
-          )
+          OffersBanners()
         ],
       ),
     );
-  }
-}
-
-class CirclePainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint= Paint()
-      ..color = Colors.blue
-      ..strokeWidth = 4.0
-      ..style = PaintingStyle.fill;
-
-    final center = Offset(size.width / 2, size.height / 2);
-    final radius = size.width / 2;
-
-    canvas.drawCircle(center, radius, paint);
-  }
-  
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return false;
   }
 }
---
+++ lib/widgets/offers_banners.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+//import 'package:food_delivery/pages/home_page.dart';
+
+class OffersBanners extends StatefulWidget {
+  const OffersBanners({super.key});
+  @override
+  State<OffersBanners> createState() => _OffersBannersState();
+}
+
+
+class _OffersBannersState extends State<OffersBanners> {
+  int? offerIndex;
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.bottomCenter,
+      children: [
+        Container(
+          height: 150,
+          child: PageView(
+            //physics: const NeverScrollableScrollPhysics(),
+            controller: PageController(),
+            onPageChanged: (index) {
+              setState(() {
+                offerIndex = index;
+              });
+              //page = index;
+            },
+            scrollDirection: Axis.horizontal,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: ClipRRect(
+                    //to clip image and make it rounded border
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.asset(
+                      'assets/images/offer2.jpg',
+                      //width: 400,
+                      //height: 200,
+                      fit: BoxFit.cover,
+                    )),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: ClipRRect(
+                    //to clip image and make it rounded border
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.asset(
+                      'assets/images/offer1.jpg',
+                      //width: 400,
+                      //height: 200,
+                      fit: BoxFit.cover,
+                    )),
+              ),
+            ],
+          ),
+        ),
+        Column(
+          children: [
+            Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: List.generate(
+                  2,
+                  (index) => offerIndex == index
+                      ? Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 2.5),
+                        child: CustomPaint(
+                            size: const Size(7, 7),
+                            painter: CirclePainter(Colors.white60),
+                          ),
+                      )
+                      : Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 2.5),
+                        child: CustomPaint(
+                            size: const Size(5, 5),
+                            painter: CirclePainter(Colors.black26),
+                          ),
+                      ),
+                )),
+            const SizedBox(
+              height: 10,
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
+
+
+class CirclePainter extends CustomPainter {
+  Color  color = Colors.black45;
+
+  CirclePainter(this.color);
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = color
+
+      ..strokeWidth = 4.0
+      ..style = PaintingStyle.fill;
+
+    final center = Offset(size.width / 2, size.height / 2);
+    final radius = size.width / 2;
+
+    canvas.drawCircle(center, radius, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+  }
+}
